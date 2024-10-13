08f9bb2611051a0b92b2e7753cf73a082d7ab8d7
Arthur Bispo <64386652+arthurxsz@users.noreply.github.com>
2024-10-13 11:13:31 -0300

    Update telegram_msg.py

--- telegram_msg.py
+++ telegram_msg.py
@@ -1,18 +1,8 @@
 import asyncio
 import telegram
 
-
 bot = telegram.Bot(token=API)
 
 async def send_message(text, chat_id = chat_id):
     async with bot:
         await bot.send_message(text=text, chat_id=chat_id)
-
-async def main():
-    # Sending a message
-    await send_message(text='LIBEROU VAGA', chat_id=chat_id)
-
-
-if __name__ == '__main__':
-    asyncio.run(main())
-    
