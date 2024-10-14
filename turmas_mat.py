b8be5d47ef1663f00ea475006e860f50f834e4f5
Arthur Bispo <64386652+arthurxsz@users.noreply.github.com>
2024-10-13 22:00:21 -0300

    Add files via upload

---
+++ turmas_mat.py
@@ -0,0 +1,69 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+import telegram_msg
+import asyncio
+
+# Configuração Chrome
+chrome_options = Options()
+# chrome_options.add_argument("--no-sandbox")
+# chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--disable-gpu")
+
+# Inicializa o WebDriver com as opções headless
+driver = webdriver.Chrome(options=chrome_options)
+
+# Acessa a página
+driver.get("https://sigaa.unb.br/sigaa/public/turmas/listar.jsf")
+
+# Aguarda o carregamento da página
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "formTurma:inputNivel")))
+
+# Seleciona "GRADUAÇÃO" no campo "Nível de ensino"
+nivel_ensino_select = Select(driver.find_element(By.NAME, "formTurma:inputNivel"))
+nivel_ensino_select.select_by_visible_text("GRADUAÇÃO")
+
+# Seleciona "DEPTO CIÊNCIAS DA COMPUTAÇÃO - BRASÍLIA" no campo "Unidade"
+unidade_select = Select(driver.find_element(By.NAME, "formTurma:inputDepto"))
+unidade_select.select_by_visible_text("DEPARTAMENTO DE MATEMÁTICA - BRASÍLIA")
+
+turmas_quero = {'MAT0053 - CALCULO NUMERICO',
+                '01', '04', '05',
+                }
+turma_atual = []
+
+while (True):
+    # Clica no botão "Buscar"
+    buscar_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//input[@value='Buscar']"))
+    )
+    buscar_button.click()
+
+    # Aguarda o carregamento da página de resultados
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "listagem"))
+    )
+
+    # Coleta as informações da tabela de turmas
+    tabela = driver.find_element(By.CLASS_NAME, "listagem")
+    linhas = tabela.find_elements(By.TAG_NAME, "tr")
+
+    # Extrai as informações de cada linha da tabela
+    for linha in linhas:
+        colunas = linha.find_elements(By.TAG_NAME, "td")
+        dados_linha = [coluna.text for coluna in colunas]
+
+        if len(dados_linha) == 1:
+            turma_atual = dados_linha
+
+        if turma_atual and turma_atual[0] in turmas_quero and dados_linha[0] in turmas_quero:
+            print(dados_linha)
+            if len(dados_linha) > 1 and int(dados_linha[6]) < int(dados_linha[5]):
+                print(f'{("LIBEROU VAGA " + turma_atual[0]+ "\n")*5}')
+                asyncio.run(telegram_msg.send_message(f'{("LIBEROU VAGA " + turma_atual[0])}'))
+
+
+# Fecha o navegador
+driver.quit()
